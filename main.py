79fe624d3da3581e3fb84df80974cfb9d7ff1bc4
𝒩𝑜𝓏𝒾𝓁𝒶 <nozilarajabboyeva@gmail.com>
2026-02-21 22:18:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+"""1–10: Parametrsiz funksiyalar"""
+# #0.1-misol
+# def salom():
+#     print("Salom, dunyo!")
+#
+# salom()
+
+# #0.2-misol
+# def son():
+#     print(100)
+#
+# son()
+
+# #0.3-misol
+# def motivatsiya():
+#     print("Siz mavaffaqiyat sari yurasiz!")
+#
+# motivatsiya()
+
+# #0.4-misol
+# def matn():
+#     print("Python " * 3)
+#
+# matn()
+
+# #0.5-misol
+# def qoshish():
+#     print(5 + 7)
+#
+# qoshish()
+
+# #0.6-misol
+# def vaqt():
+#     print("Hozir tong")
+#
+# vaqt()
+
+# #0.8-misol
+# def xabar():
+#     print("Dastur ishga tushdi!")
+#
+# xabar()
+
+# #0.9-misol
+# def tasodifiy_salom():
+#     print("Bugun yaxshi kun!")
+#
+# tasodifiy_salom()
+
+# #1.0-misol
+# def oddiy():
+#     print("Xush kelibsiz!")
+#
+# oddiy()
